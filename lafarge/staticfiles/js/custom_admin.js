ba505d9fd58e80e8ddd8cbc9a85c5bf2a956b4dc
mfcc168 <88373848+mfcc168@users.noreply.github.com>
2025-04-09 11:45:46 +0800

    delete

--- lafarge/staticfiles/js/custom_admin.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    $(document).on("shown.bs.modal", ".modal", function () {
-        $(this).modal({
-            backdrop: "static",  // Prevents closing when clicking outside
-            keyboard: false      // Prevents closing with ESC key
-        });
-    });
-});
