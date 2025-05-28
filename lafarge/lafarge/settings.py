40c5c9e065afb8cbdcfd9db0bcc26a62170cd5ef
mfcc168 <88373848+mfcc168@users.noreply.github.com>
2025-05-28 11:14:56 +0800

    use new database

--- lafarge/lafarge/settings.py
+++ lafarge/lafarge/settings.py
@@ -63,7 +63,7 @@ WSGI_APPLICATION = 'lafarge.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database.sqlite3',
     }
 }
 
